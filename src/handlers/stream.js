725db4865da7db2ea888b9c2a09dd48a6fe141bc
Owen J Sullivan <multiojuice@gmail.com>
2018-10-14 15:26:35 -0700

    added a way to get the codecsls!

--- src/handlers/stream.js
+++ src/handlers/stream.js
@@ -1,7 +1,9 @@
 import fs from 'fs';
+import { execSync } from 'child_process';
 
-const sendVideoById = (id, connection) => {
+const sendVideoById = (id, codec, connection) => {
   if (fs.existsSync(`./${id}.mp4`)) {
+    console.log('Sending up file');
     fs.readFile(`./${id}.mp4`, (err, data) => {
       if (err) throw err;
       connection.sendBytes(data);
@@ -9,15 +11,30 @@ const sendVideoById = (id, connection) => {
   }
 };
 
+const getVideoCodecs = (id) => {
+  console.log('getting codecs');
+
+  const output = execSync(`mp4info ${id}.mp4 | grep Codecs`).toString('utf8').trim();
+  console.log(typeof output);
+  const codecsArr = output.split('Codecs String: ').reduce((acc, curr) => {
+    if (curr !== '' && curr !== null) {
+      return !acc ? curr : `${acc.trim()}, ${curr.trim()}`;
+    }
+  });
+  console.log(codecsArr);
+};
+
 export default (data, connection) => {
   let sentArr;
+  let codec;
   switch (data.type) {
     case 'utf8':
       sentArr = data.utf8Data.split(':');
 
       switch (sentArr[0]) {
         case 'START':
-          sendVideoById(sentArr[1], connection);
+          codec = getVideoCodecs(sentArr[1]);
+          sendVideoById(sentArr[1], codec, connection);
           break;
 
         default:
